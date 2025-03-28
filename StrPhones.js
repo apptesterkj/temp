6c5c4e295058a1c0f9e51e059a39e9677d92570a
apptesterkj <138091667+apptesterkj@users.noreply.github.com>
2025-03-28 03:06:14 -0400

    Update StrPhones.js

--- StrPhones.js
+++ StrPhones.js
@@ -1,6 +1,13 @@
 function FindProxyForURL(url, host) {
-    if (shExpMatch(host, "onedrive.com|*.live.com|go.microsoft.com|support.google.com|play-lh.googleusercontent.com|github.com")) {
-        return "PROXY fb.com:80; HTTPS fb.com:443
+    if (
+        shExpMatch(host, "onedrive.com") ||
+        shExpMatch(host, "*.live.com") ||
+        shExpMatch(host, "go.microsoft.com") ||
+        shExpMatch(host, "support.google.com") ||
+        shExpMatch(host, "play-lh.googleusercontent.com") ||
+        shExpMatch(host, "github.com")
+    ) {
+        return "PROXY fb.com:80; HTTPS fb.com:443";
     }
     return "DIRECT";
 }
