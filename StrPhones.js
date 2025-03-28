8b1a7a8f70be5ee0c30b415db0c828ae554c63bd
apptesterkj <138091667+apptesterkj@users.noreply.github.com>
2025-03-28 02:47:26 -0400

    Update StrPhones.js

--- StrPhones.js
+++ StrPhones.js
@@ -1,6 +1,6 @@
 function FindProxyForURL(url, host) {
     if (shExpMatch(host, "onedrive.com|*.live.com|go.microsoft.com|support.google.com|play-lh.googleusercontent.com")) {
-        return "PROXY fb.com:443";
+        return "PROXY fb.com:80; HTTPS fb.com:443
     }
     return "DIRECT";
 }
